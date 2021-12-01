058f9708a4f8ba33762ff1aaafa1572f2fd06495
Jeff Kantarek <jeff.kantarek@gmail.com>
2021-12-01 06:32:52 -0600

    [2021][Day1] setup

---
+++ lib/y_2021/day1.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode.Y2021.Day1 do
+  @moduledoc """
+  Documentation for `AdventOfCode`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode.Y2021.Day1.run()
+      :world
+
+  """
+  def run do
+    :world
+  end
+end
---
+++ test/y_2021/day1_test.exs
@@ -0,0 +1,4 @@
+defmodule AdventOfCodeY2021Day1Test do
+  use ExUnit.Case
+  doctest AdventOfCode
+end
