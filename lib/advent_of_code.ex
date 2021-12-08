c691510a8b8852cf7577140de4aa06f1e1479039
Jeff Kantarek <jeff.kantarek@gmail.com>
2021-12-07 19:20:52 -0600

    refactor allow new split functions

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -11,20 +11,29 @@ defmodule AdventOfCode do
       iex> AdventOfCode.etl_file("lib/input.txt", fn(s) -> s end)
       ["157", "148", "149"]
 
-      iex> AdventOfCode.etl_file("lib/input.txt", fn(s) -> s end, %{reject_blanks: false, reject_nils: true})
+      iex> AdventOfCode.etl_file("lib/input.txt", fn(s) -> s end, &AdventOfCode.split_newline/1, %{reject_blanks: false, reject_nils: true})
       ["157", "148", "149", ""]
 
   """
-  def etl_file(filename, etl_func, opts \\ %{reject_blanks: true, reject_nils: true}) do
+  def etl_file(
+        filename,
+        etl_func,
+        split_func \\ &split_newline/1,
+        opts \\ %{reject_blanks: true, reject_nils: true}
+      ) do
     {:ok, file} = File.read(filename)
 
     file
-    |> String.split("\n")
+    |> split_func.()
     |> Enum.reject(fn elem ->
       (opts[:reject_blanks] && elem == "") || (opts[:reject_nils] && is_nil(elem))
     end)
     |> Enum.map(fn s ->
       etl_func.(s)
     end)
+  end
+
+  def split_newline(v) do
+    v |> String.split("\n")
   end
 end
