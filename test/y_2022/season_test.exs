6353b5bfbda0539912514c1b9693970acb9d2e28
jkantarek <10119570+jkantarek@users.noreply.github.com>
2022-12-07 06:33:10 -0600

    [2022][d6] part 1 and 2

--- test/y_2022/season_test.exs
+++ test/y_2022/season_test.exs
@@ -5,4 +5,5 @@ defmodule AdventOfCodeY2022Day1Test do
   doctest AdventOfCode.Y2022.Day3
   doctest AdventOfCode.Y2022.Day4
   doctest AdventOfCode.Y2022.Day5
+  doctest AdventOfCode.Y2022.Day6
 end
